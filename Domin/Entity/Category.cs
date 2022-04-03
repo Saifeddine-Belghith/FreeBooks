0396b49da764e4752f7a6223d067a079269bc3fa
GHOST <saifeddinebelghith12@gmail.com>
2022-04-03 04:41:34 +0100

    Ajoutez des fichiers projet.

---
+++ Domin/Entity/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domin.Entity
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+
+        [Required(ErrorMessageResourceType =typeof(Resource.ResourceData),ErrorMessageResourceName ="CategoryName")]
+        [MaxLength(20,ErrorMessageResourceType =typeof(Resource.ResourceData),ErrorMessageResourceName ="Length")]
+        [MinLength(3,ErrorMessageResourceType =typeof(Resource.ResourceData),ErrorMessageResourceName ="MinLength")]
+        public string Name { get; set; }
+
+
+        public string Description { get; set; }
+        public int CurrentStatut { get; set; }
+    }
+}
